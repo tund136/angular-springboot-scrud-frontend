982d10b53dc82162bcb75273fdee1746d5c82dc3
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2021-04-14 20:48:53 +0700

    Creating Angular Create Employee Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { from } from 'rxjs';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeListComponent],
+  declarations: [AppComponent, EmployeeListComponent, CreateEmployeeComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
